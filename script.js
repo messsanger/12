82d3b4877a580f858cc09b00dfe505490475e673
messsanger <164818279+messsanger@users.noreply.github.com>
2024-04-09 09:22:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function replaceImage() {  
+    var imageElement = document.getElementById('image');  
+    imageElement.src = 'j.jpg'; // Replace with the new image source  
+    imageElement.alt = 'New Image'; // Update alt text if needed  
+}
